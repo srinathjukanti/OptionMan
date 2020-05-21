bf5af5a9c0bd30c1b28a5655d9319fc02e376236
sj <sj@gmail.com>
2020-05-21 17:53:43 +0000

    yahoo finance api

--- option_helpers.py
+++ option_helpers.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import copy
+import yfinance as yf
 from wallstreet import Call, Put, Stock
 
 def find_closest_strike(strikes, stock_price):
@@ -35,7 +36,7 @@ def get_option(ticker, d=12, m=2, y=2016, option_type=Call):
   except:
     raise
 
-def get_holdings_iv(holdings, limit=100, highIv=True):
+def get_stats(holdings, limit=100):
   stock_list = []
   exceptions = []
   for ticker in holdings[:limit]:
@@ -43,7 +44,9 @@ def get_holdings_iv(holdings, limit=100, highIv=True):
       option = get_option(ticker)
       stock = option.underlying
       iv, strike = get_stock_iv(option, stock.price)
-      stock_list.append((iv, strike, ticker))
+      msft = yf.Ticker('MSFT')
+      marketCap = msft.info['marketCap']
+      stock_list.append((ticker, marketCap, iv, strike))
     except Exception as e:
       exceptions.append((ticker, str(e)))
   return stock_list, exceptions
--- option_man.py
+++ option_man.py
@@ -1,13 +1,34 @@
 import pandas as pd
-from option_helpers import get_holdings_iv
+import yfinance as yf
+import json
+from option_helpers import get_stats
 from pathlib import Path
 
 def get_holdings_csv(name="sp500-constituents.csv"):
     holdings_dir = Path("holdings/")
     sp500_holdings = holdings_dir / name
 
-# holdings_df = pd.read_csv(sp500_holdings)
-# sp500_tickers = list(holdings_df['Symbol'])
+def tickers():
+    holdings_csv = get_holdings_csv()
+    holdings_df = pd.read_csv(holdings_csv)
+    tickers = list(holdings_df['Symbol'])
+    return tickers
+
+def calcStockStats(tickers):
+    stocks, exceptions = get_stats(tickers, limit=len(tickers))
+    return stocks, exceptions
+
+def filterHighMarketCap(stocks, limit=100):
+    stocks.sort(key=lambda x:x[1], reverse=True) 
+    return stocks[:limit]
+
+stocks = calcStockStats(tickers())
+stocks = filterHighMarketCap(stocks)
+stocks.sort(key=lambda x:x[2], reverse=True)
+
+stocks_json = json.dumps(dict(stocks))
+print(stocks_json)
+
 # stocks, exceptions = get_holdings_iv(holdings=sp500_tickers, limit=10)
 # highIvFirst=True
 # stocks.sort(key=lambda x:x[0], reverse=highIvFirst)
